a4a8cbf57fbbe82a638789ec83f5ff3cf0fbc330
bhsueh <bhsueh@nvidia.com>
2020-10-26 17:45:20 +0800

    feat: Add bert, and mutiply the gemm cost by 2 due to MMA operation.

--- bert.py
+++ bert.py
@@ -3,8 +3,8 @@ from utils.gpu import gpu_a100
 from utils.gpu import gpu_t4
 from utils.layer import Layer
 
-class Gpt2_Model:
-    def __init__(self, name, gpu, batch, seq, head, size, dtype, num_layer, vocab):
+class Bert_Model:
+    def __init__(self, name, gpu, batch, seq, head, size, dtype, num_layer):
         self.name = name
         self.gpu = gpu
         self.batch = batch
@@ -15,18 +15,14 @@ class Gpt2_Model:
         self.dtype = dtype
         self.dtype_byte= self.dtype / 8
         self.num_layer = num_layer
-        self.vocab = vocab
         
         self.build_model()
         self.print_sol()
     
     def build_model(self):
         self.layers = []
-        self.layers.append(Layer("embedding_lookup",
-                                 comp_cost = 0,
-                                 mem_cost = self.dtype_byte * (self.vocab * self.hidden + self.batch * self.hidden) ))
-        
-        m = self.batch
+
+        m = self.batch * self.seq
         k = self.hidden
         n = self.hidden
         self.layers.append(Layer("transformer/query", 
@@ -41,8 +37,8 @@ class Gpt2_Model:
         
         # multihead attention uses "(1 + self.seq) / 2 because the size grows from 1 to self.seq"
         self.layers.append(Layer("transformer/multihead_attention",
-                                 comp_cost = self.batch * self.head * (self.size * 2 * (1 + self.seq) / 2), # only consider batch gemm ignore softmax cost
-                                 mem_cost = self.num_layer * self.dtype_byte * (2 * m * n + 2 * m * (1 + self.seq) / 2 * n)))
+                                 comp_cost = 0, # self.batch * self.head * (self.size * 2 * (1 + self.seq) / 2), # only consider batch gemm ignore softmax cost
+                                 mem_cost = 0)) #self.num_layer * self.dtype_byte * (2 * m * n + 2 * m * (1 + self.seq) / 2 * n)))
         self.layers.append(Layer("transformer/atten_out", 
                                 comp_cost = self.num_layer * m * k * n * 2,
                                 mem_cost = self.num_layer * self.dtype_byte * (m * k + k * n + m * n)))
@@ -61,32 +57,16 @@ class Gpt2_Model:
                                  mem_cost = self.num_layer * self.dtype_byte * (2 * m * n + n)))
         k = 4 * self.hidden
         n = self.hidden
-        self.layers.append(Layer("transformer/ffn gemm 2" * 2,
-                                comp_cost = self.num_layer * m * k * n,
+        self.layers.append(Layer("transformer/ffn gemm 2", 
+                                comp_cost = self.num_layer * m * k * n * 2,
                                 mem_cost = self.num_layer * self.dtype_byte * (m * k + k * n + m * n)))
         k = self.hidden
         self.layers.append(Layer("transformer/reduce_communication",
                                  comp_cost = 0,
                                  mem_cost = 0))
-        self.layers.append(Layer("transformer/add_bias_input",
+        self.layers.append(Layer("transformer/add_bias_input_layernorm",
                                  comp_cost = self.num_layer * 2 * m * n,
                                  mem_cost = self.num_layer * self.dtype_byte * (3 * m * n + n)))
-        
-        self.layers.append(Layer("layer_norm",
-                                 comp_cost = 0,
-                                 mem_cost = 0))
-        self.layers.append(Layer("compute_logits",
-                                 comp_cost = self.batch * self.hidden * self.vocab,
-                                 mem_cost = self.dtype_byte * (self.batch * self.hidden + self.hidden * self.vocab + self.batch * self.vocab) ))
-        self.layers.append(Layer("top k",
-                                 comp_cost = 0,
-                                 mem_cost = 0))
-        self.layers.append(Layer("softmax",
-                                 comp_cost = 0,
-                                 mem_cost = 0))
-        self.layers.append(Layer("top p",
-                                 comp_cost = 0,
-                                 mem_cost = 0))
         
     def print_sol(self):
         total_time_per_step = 0
@@ -100,13 +80,10 @@ class Gpt2_Model:
             
         parameters_num = (12 * self.hidden * self.hidden + 15 * self.hidden) * self.num_layer
         print("{} model".format(self.name))
-        print("    Model size: {:5.2f} billion parameters, {} GBs. ".format(parameters_num / 1e9, parameters_num * self.dtype_byte / 1024 / 1024 /1024))
+        print("    Model size: {:5.2f} million parameters, {} MBs. ".format(parameters_num / 1e6, parameters_num * self.dtype_byte / 1024 / 1024))
         
-        print("    Model time: time cost per step: {:10.6} ms; total time cost: {:10.3} ms \n".format(total_time_per_step / 1000, total_time_per_step * self.seq / 1000))
+        print("    Model time: total time cost: {:10.3} ms \n".format(total_time_per_step / 1000))
 
 if __name__ == "__main__":
-    # gpt2_model = Gpt2_Model(name="GPT-3-v1", gpu=gpu_v100, batch=8, seq=1024, head=56, size=128, dtype=16, num_layer=32, vocab=50257)
-    # gpt2_model = Gpt2_Model(name="GPT-3-v1", gpu=gpu_v100, batch=8, seq=1024, head=96, size=128, dtype=16, num_layer=96, vocab=50257)
-    gpt2_model = Gpt2_Model(name="GPT-3 60B-1", gpu=gpu_v100, batch=8, seq=1024, head=96, size=128, dtype=16, num_layer=32, vocab=50257)
-    gpt2_model = Gpt2_Model(name="GPT-3 60B-2", gpu=gpu_v100, batch=8, seq=1024, head=56, size=128, dtype=16, num_layer=96, vocab=50257)
+    gpt2_model = Bert_Model(name="BERT-base", gpu=gpu_v100, batch=128, seq=128, head=12, size=64, dtype=16, num_layer=1)
     
